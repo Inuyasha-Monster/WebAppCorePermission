9217b91e2fc62284228e7f6a2ac351518c9c5f71
杜江龙 <djlnet@outlook.com>
2016-10-03 21:33:32 +0800

    添加项目文件。

---
+++ src/WebAppCorePermission/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppCorePermission
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                //开发环境异常处理
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //生产环境异常处理
+                app.UseExceptionHandler("/Shared/Error");
+            }
+            //使用静态文件
+            app.UseStaticFiles();
+
+            app.UseMvc(routers =>
+            {
+                routers.MapRoute(
+                    "default",
+                    "{controller=Login}/{action=Index}/{id?}");
+            });
+
+            //app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
