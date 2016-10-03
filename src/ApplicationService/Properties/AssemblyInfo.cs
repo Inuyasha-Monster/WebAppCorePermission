6a0e3b7374a72d4953989971a0f58b90de69a688
杜江龙 <djlnet@outlook.com>
2016-10-03 23:28:12 +0800

    添加服务、工具类库

---
+++ src/ApplicationService/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApplicationService")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a249197e-ed9c-46f6-913d-71fcff3204e5")]
---
+++ src/ApplicationService/UserService/UserAppService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.IRepositories;
+
+namespace ApplicationService.UserService
+{
+    public class UserAppService : IUserAppService
+    {
+        //用户管理仓储接口
+        private readonly IUserRepository _userReporitory;
+
+        /// <summary>
+        /// 构造函数 实现依赖注入
+        /// </summary>
+        /// <param name="userRepository">仓储对象</param>
+        public UserAppService(IUserRepository userRepository)
+        {
+            this._userReporitory = userRepository;
+        }
+
+        public User CheckUser(string userName, string password)
+        {
+            return _userReporitory.CheckUser(userName, password);
+        }
+    }
+}
