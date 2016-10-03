18406c6d83f5cbeee4cb9a01083cefcc21b913e0
杜江龙 <djlnet@outlook.com>
2016-10-03 22:35:48 +0800

    权限实体

---
+++ src/Domain/Entities/Role.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// 角色实体
+    /// </summary>
+    public class Role : BaseEntity
+    {
+        /// <summary>
+        /// 角色编码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 角色名字
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 创建人Id
+        /// </summary>
+        public Guid CreateUserId { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remarks { get; set; }
+
+        public virtual User CreateUser { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
---
+++ src/Domain/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    /// <summary>
+    /// 泛型基类实体
+    /// </summary>
+    /// <typeparam name="TPrimaryKey"></typeparam>
+    public abstract class Entity<TPrimaryKey>
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public TPrimaryKey Id { get; set; }
+    }
+}
